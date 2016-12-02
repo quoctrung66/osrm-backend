d8e86ad70fdac0dc5817b7d4923a91b69d2f0d39
Daniel Patterson <daniel@mapbox.com>
2016-12-02 02:16:34 -0800

    Fix strlen include.

--- include/util/source_macros.hpp
+++ include/util/source_macros.hpp
@@ -1,7 +1,8 @@
 #ifndef SOURCE_MACROS_HPP
 #define SOURCE_MACROS_HPP
+#include <cstring>
 
-#define PROJECT_RELATIVE_PATH(x) std::string(x).substr(std::strlen(OSRM_PROJECT_DIR) + 1)
+#define PROJECT_RELATIVE_PATH(x) std::string(x).substr(strlen(OSRM_PROJECT_DIR) + 1)
 #define OSRM_SOURCE_FILE PROJECT_RELATIVE_PATH(__FILE__)
 
 #endif // SOURCE_MACROS_HPP
